0ba85cb0e8c8165a5bdf28ad5b97489e31c7b543
Isan-Rivkin <isanrivkin@gmail.com>
2018-10-05 14:51:24 +0300

    DEV: initial Synchronizer,Ethereum, DB

---
+++ src/ethereum/EnigmaContractAPI.js
@@ -0,0 +1,30 @@
+class EnigmaContract{
+    constructor(){
+
+    }
+    getContractsNum(){
+
+    }
+    getDeltasNum(contractAddr){
+
+    }
+    isSecretContract(addr){
+
+    }
+    isValidDelta(addr, deltaHash){
+
+    }
+    isValidDeltaRange(addr, deltaHashesList){
+
+    }
+    getSecretContractByteCodeHash(addr){
+
+    }
+    getDeltaAt(addr,deltaIndex){
+
+    }
+    getDeltasRange(addr, indexRange){
+
+    }
+
+}
