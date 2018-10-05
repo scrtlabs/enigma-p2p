afd1719afa0a5e131660421c4973b68aebd8f62c
Isan-Rivkin <isanrivkin@gmail.com>
2018-10-05 21:13:25 +0300

    DEV: Synchronizer placeholder

--- src/worker/state_sync/Synchronizer.js
+++ src/worker/state_sync/Synchronizer.js
@@ -1,3 +1,14 @@
+
 class Synchronizer{
 
+    constructor(enigmaNode){
+        this._enigmaNode = enigmaNode;
+    }
+    /** announce the network the CID's that are provided
+     * @param {Array<EngCID>} cidList,
+     * @param {Function} onDone, (erroredCID)=>{}
+     * */
+    provide(engCIDList, onDone){}
+
+    /** */
 }
