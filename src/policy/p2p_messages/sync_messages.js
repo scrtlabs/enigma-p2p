e5183df68d0f474c9a94367d3e7349a73fbfa9ea
Isan-Rivkin <isanrivkin@gmail.com>
2018-10-25 18:35:34 +0300

    DEV: comleted builder for state+bcode sync req,res msgs

--- src/policy/p2p_messages/sync_messages.js
+++ src/policy/p2p_messages/sync_messages.js
@@ -5,6 +5,11 @@ const nodeUtils = require('../../common/utils');
 const EncoderUtil = require('../../common/EncoderUtil');
 
 class SyncMsgBuilder {
+    /**
+     * from network stream
+     * @param {Array<Integer>} networkMsg , a StateSyncReq msg
+     * @param {Function} callback, (err,SyncStateReqMsg)=>{}
+     * */
     static stateReqFromNetwork(networkMsg, callback){
         let obj = SyncMsgBuilder._parseFromNetwork(networkMsg);
         if(obj){
@@ -26,20 +31,51 @@ class SyncMsgBuilder {
         let obj = JSON.parse(decoded);
         return obj;
     }
+    /**
+     * from regular object in the code (JSON)
+     * @param {Json} msgObj , a StateSyncReq msg
+     * @param {Function} callback, (err,SyncStateReqMsg)=>{}
+     * */
     static stateReqFromObj(msgObj,callback){
-        msgObj.msgType = 'SYNC_STATE_REQ';
+        msgObj.msgType = MSG_TYPES.SYNC_BCODE_REQ;
         SyncMsgBuilder._buildMsg(MSG_TYPES.SYNC_STATE_REQ, msgObj, (err,message)=>{
             callback(err,message);
         });
     }
     static stateResFromObj(msgObj,callback){
-        msgObj.msgType = 'SYNC_STATE_RES';
+        msgObj.msgType = MSG_TYPES.SYNC_STATE_RES;
         SyncMsgBuilder._buildMsg(MSG_TYPES.SYNC_STATE_RES, msgObj, (err,message)=>{
             callback(err,message);
         });
     }
-    static bCodeFromNetwork(){}
-    static bCodeFromObj(){}
+    static bCodeReqFromNetwork(networkMsg,callback){
+        let obj = SyncMsgBuilder._parseFromNetwork(networkMsg);
+        if(obj){
+            SyncMsgBuilder.bCodeReqFromObj(obj,callback);
+        }else{
+            callback('error decoded network msg');
+        }
+    }
+    static bCodeReqFromObj(msgObj,callback){
+        msgObj.msgType = MSG_TYPES.SYNC_BCODE_REQ;
+        SyncMsgBuilder._buildMsg(MSG_TYPES.SYNC_BCODE_REQ, msgObj, (err,message)=>{
+            callback(err,message);
+        });
+    }
+    static bCodeResFromNetwork(networkMsg,callback){
+        let obj = SyncMsgBuilder._parseFromNetwork(networkMsg);
+        if(obj){
+            SyncMsgBuilder.bCodeResFromObj(obj,callback);
+        }else{
+            callback('error decoded network msg');
+        }
+    }
+    static bCodeResFromObj(msgObj, callback){
+        msgObj.msgType = MSG_TYPES.SYNC_BCODE_RES;
+        SyncMsgBuilder._buildMsg(MSG_TYPES.SYNC_BCODE_RES, msgObj, (err,message)=>{
+            callback(err,message);
+        });
+    }
     static _isValidScheme(schemeType,testedObj,callback){
         schemeValidator.validateScheme(testedObj, schemeType,callback);
     }
@@ -55,6 +91,12 @@ class SyncMsgBuilder {
                             break;
                         case MSG_TYPES.SYNC_STATE_RES:
                             callback(null,new SyncStateResMsg(msgObj));
+                            break;
+                        case MSG_TYPES.SYNC_BCODE_REQ:
+                            callback(null, new SyncBcodeReqMsg(msgObj));
+                            break;
+                        case MSG_TYPES.SYNC_BCODE_RES:
+                            callback(null, new SyncBcodeResMsg(msgObj));
                             break;
                     }
                 }else{
@@ -75,6 +117,8 @@ class SyncMsg {
     toPrettyJSON(){
         return JSON.stringify(this._rawMsg,null,2);
     }
+    /** before sending to network;
+     * @returns {Array<Integer>} encoded and seriallized msgpack array of the msg*/
     toNetwork(){
         let msg = this.toJSON();
         let encoded = EncoderUtil.encodeToNetwork(msg);
@@ -82,6 +126,9 @@ class SyncMsg {
     }
 }
 
+/**
+ * A Message request representing the range of deltas required.
+ * */
 class SyncStateReqMsg extends SyncMsg{
     constructor(rawMsg){
         super(rawMsg);
@@ -111,7 +158,9 @@ class SyncStateReqMsg extends SyncMsg{
     }
 }
 
-
+/**
+ * A Message response to a state sync request of deltas
+ * */
 class SyncStateResMsg extends SyncMsg{
     constructor(rawMsg){
         super(rawMsg);
@@ -127,7 +176,7 @@ class SyncStateResMsg extends SyncMsg{
             return this.states()[index];
         }
     }
-    orderStates(){
+    sortStates(){
         this.states().sort((d1,d2)=>{
             return d1.index - d2.index;
         });
@@ -135,10 +184,73 @@ class SyncStateResMsg extends SyncMsg{
     }
 }
 
+class SyncBcodeReqMsg extends SyncMsg{
+    constructor(rawMsg){
+        super(rawMsg);
+    }
+    contractAddress(){
+        return this._rawMsg.contractAddress;
+    }
+}
+
+class SyncBcodeResMsg extends SyncMsg{
+    constructor(rawMsg){
+        super(rawMsg);
+    }
+    contractAddress(){
+        return this._rawMsg.contractAddress;
+    }
+    deployedBytecode(){
+        return this._rawMsg.deployedBytecode;
+    }
+}
+
+
+
 module.exports.SyncStateResMsg = SyncStateResMsg;
 module.exports.SyncStateReqMsg = SyncStateReqMsg;
 module.exports.SyncMsgBuilder = SyncMsgBuilder;
+module.exports.SyncBcodeReqMsg = SyncBcodeReqMsg;
+module.exports.SyncBcodeResMsg = SyncBcodeResMsg;
+
 /** mini tests */
+
+
+/** bcode sync */
+
+// let state_bcode_req = {
+//     contractAddress : '0x1...',
+// };
+
+// let state_bcode_res = {
+//     contractAddress : '0x1...',
+//     deployedByteCode : [11,12,13,15,16,13,111,133,662]
+// };
+//
+// SyncMsgBuilder.bCodeReqFromObj(state_bcode_res, (err,msg)=>{
+//     if(err){
+//         console.log("err " + err );
+//     }else{
+//         console.log(msg.toPrettyJSON());
+//         let encoded = msg.toNetwork();
+//         SyncMsgBuilder.bCodeResFromNetwork(encoded,(err,msg2)=>{
+//             console.log(msg.toPrettyJSON());
+//         });
+//     }
+// });
+//
+// SyncMsgBuilder.bCodeReqFromObj(state_bcode_req,(err,msg)=>{
+//     if(err){
+//         console.log("Err " + err);
+//     }else{
+//         console.log(msg.toPrettyJSON());
+//         let encoded = msg.toNetwork();
+//         SyncMsgBuilder.bCodeReqFromNetwork(encoded , (err,msg2)=>{
+//             console.log(msg2.toPrettyJSON());
+//         });
+//     }
+// });
+/** states/deltas sync */
 //
 // let state_sync_req_obj = {
 //     contractAddress : '0x...',
@@ -172,14 +284,14 @@ module.exports.SyncMsgBuilder = SyncMsgBuilder;
 //
 // SyncMsgBuilder.stateResFromObj(state_sync_res_obj, (err,msg)=>{
 //     // console.log("err res ? " + err);
-//     // console.log("stats orderd  : " + JSON.stringify(msg.orderStates()));
+//     // console.log("stats orderd  : " + JSON.stringify(msg.sortStates()));
 //     // console.log("raw "  + msg.toPrettyJSON());
 //
 //     let encoded = msg.toNetwork();
 //     console.log("encoded:" + encoded);
 //     // SyncMsgBuilder.stateResFromNetwork(encoded, (err, msg2)=>{
 //     //     console.log("raw" + msg2.toPrettyJSON());
-//     //     msg2.orderStates();
+//     //     msg2.sortStates();
 //     //     console.log("raw" + msg2.toPrettyJSON());
 //     // });
 // });
