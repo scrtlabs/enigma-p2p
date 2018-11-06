7707f5b716eb66d52d6d73b444fb26e71f9360a3
Isan-Rivkin <isanrivkin@gmail.com>
2018-11-07 00:42:33 +0200

    DEV: StateCache WIP

--- src/db/LevelDbApi.js
+++ src/db/LevelDbApi.js
@@ -32,10 +32,14 @@ class LevelDbApi{
             callback("db closed");
         }
     }
-    get(key,callback){
+        get(key,callback){
         if(this._isOpen()){
             this._db.get(key,(err,value)=>{
-                callback(err,value);
+                if(err){
+                  callback(err,value);
+                }else{
+                  callback(err,JSON.parse(value));
+                }
             });
         }else{
             callback("db closed");
@@ -78,27 +82,6 @@ class LevelDbApi{
         }
     }
 
-    getAllStream(callback){
-        if(this._isOpen()){
-            this._db.createReadStream()
-                .on('data', function (data) {
-                    callback("data",data.key,data.value);
-                })
-                .on('error', function (err) {
-                    callback("error",err);
-                })
-                .on('close', function () {
-                    callback("close");
-                })
-                .on('end', function () {
-                    callback("end");
-                });
-        }else{
-            callback("close");
-        }
-
-    }
-
 }
 module.exports = LevelDbApi;
 
@@ -107,16 +90,31 @@ module.exports = LevelDbApi;
  */
 // let hexKey = '0xd00fb2b59610c1dc98929e0891b4ef3bba493d18e39e6d4eb949c811ccc52944';
 // let byteKey = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 68];
-// let byteKey2 = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 68];
-// let byteKey3 = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 68];
+// let byteKey2 = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 61];
+// let byteKey3 = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 62];
+// let byteKey4 = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 63];
+// let byteKey5 = [208, 15, 178, 181, 150, 16, 193, 220, 152, 146, 158, 8, 145, 180, 239, 59, 186, 73, 61, 24, 227, 158, 109, 78, 185, 73, 200, 17, 204, 197, 41, 64];
 // let db = new LevelDbApi("./here");
 // db.open();
-// db.put(byteKey, JSON.stringify({deltas : [byteKey2, byteKey3]}, (err)=>{
+// db.put(byteKey, JSON.stringify({deltas : [byteKey2, byteKey3]}), (err)=>{
 //     console.log("is err? ");
 //
 //     db.get(byteKey, (err,value)=>{
 //         console.log("is err? " + err);
-//         console.log(JSON.parse(value).deltas.length);
+//
+//         console.log(value.deltas.length);
+//         value.deltas.push(byteKey4);
+//         value.deltas.push(byteKey5);
+//         db.put(byteKey,JSON.stringify(value),(err)=>{
+//           console.log("is err? " + err);
+//
+//           db.get(byteKey, (err,value)=>{
+//             console.log(value.deltas);
+//           });
+//
+//         });
 //     })
 // });
+
+
 
