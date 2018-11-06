1252e20d09e885c26cc8115a6c6dfeb0cb7226cb
Isan-Rivkin <isanrivkin@gmail.com>
2018-11-06 22:42:23 +0200

    TST: updated test_tree for zmq

--- test/test_tree.js
+++ test/test_tree.js
@@ -1,8 +1,12 @@
 module.exports.TEST_TREE = {
-    'basic' : {
-        'all' : true,
-        '#1' : false,
-        '#2' : true,
-        '#3' : true,
-    },
+  'basic' : {
+    'all' : true,
+    '#1' : true,
+    '#2' : true,
+    '#3' : true,
+  },
+  'ipc' :{
+    'all' : true,
+    '#1' : true
+  }
 };
