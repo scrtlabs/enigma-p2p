86da46ca4430ca49ca858f0da7794f8a73a864b6
Isan-Rivkin <isanrivkin@gmail.com>
2018-11-08 22:18:19 +0200

    DEV: RuntimeBuilder WIP

--- src/entry_point/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is a script that wrapps everything together
- * should be used by index.js
- * shouldn't be called directly.
- * */
-
-const MainController = require('../main_controller/MainController');
-const NodeController = require('../worker/controller/NodeController');
-
