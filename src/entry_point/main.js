36a0e15ac26a9100ad78170f298cd401eabd2658
Isan-Rivkin <isanrivkin@gmail.com>
2018-11-08 19:14:18 +0200

    DEV: added entry point WIP

---
+++ src/entry_point/main.js
@@ -0,0 +1,9 @@
+/**
+ * This is a script that wrapps everything together
+ * should be used by index.js
+ * shouldn't be called directly.
+ * */
+
+const MainController = require('../main_controller/MainController');
+const NodeController = require('../worker/controller/NodeController');
+
