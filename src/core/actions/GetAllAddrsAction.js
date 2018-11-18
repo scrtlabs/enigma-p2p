4c7b45c6f38f88a0bbc79a05a0a19b91bd045ae9
Isan-Rivkin <isanrivkin@gmail.com>
2018-11-18 18:12:47 +0200

    DEV: working on sync

--- src/core/actions/GetAllAddrsAction.js
+++ src/core/actions/GetAllAddrsAction.js
@@ -7,15 +7,16 @@ class GetAllAddrsAction{
     this._coreRuntime = coreRuntime;
   }
   execute(envelop){
-    let client = this._coreRuntime.getIpcClient();
-    client.sendJsonAndReceive({
+    /***/
+    let request = {
       id : nodeUtils.randId(),
       type : Msg.GetAllAddrs
-    },(responseMsg)=>{
-      const resEnv = new Envelop(envelop.id(),responseMsg, envelop.type());
-      this._coreRuntime.getCommunicator()
-      .send(resEnv);
+    };
+    this._coreRuntime.execCmd(Msg.CORE_DB_READ_ACTION,{
+      envelop : envelop,
+      sendMsg : request,
     });
+    /***/
   }
 }
 module.exports = GetAllAddrsAction;
--- src/core/actions/GetDeltasAction.js
+++ src/core/actions/GetDeltasAction.js
@@ -7,16 +7,18 @@ class GetDeltasAction{
     this._coreRuntime = coreRuntime;
   }
   execute(envelop){
-    let client = this._coreRuntime.getIpcClient();
-    client.sendJsonAndReceive({
+
+    /***/
+    let request = {
       id : nodeUtils.randId(),
       type : Msg.GetDeltas,
       input : envelop.content().input,
-    },(responseMsg)=>{
-      const resEnv = new Envelop(envelop.id(),responseMsg, envelop.type());
-      this._coreRuntime.getCommunicator()
-      .send(resEnv);
+    };
+    this._coreRuntime.execCmd(Msg.CORE_DB_READ_ACTION,{
+      envelop : envelop,
+      sendMsg : request,
     });
+    /***/
   }
 }
 module.exports = GetDeltasAction;
