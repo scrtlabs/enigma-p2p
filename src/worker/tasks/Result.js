f607242a8fe437e4da32e941e88745f65f07d8c7
Isan-Rivkin <isanrivkin@gmail.com>
2019-01-22 20:07:59 +0200

    DEV: combining Result classes

--- src/worker/tasks/Result.js
+++ src/worker/tasks/Result.js
@@ -4,8 +4,6 @@ class Result{
   constructor(taskId,status){
     this._taskId = taskId;
     this._status = status;
-    this._SUCCESS = constants.TASK_STATUS.SUCCESS;
-    this._FAILED = constants.TASK_STATUS.FAILED;
   }
   getTaskId(){
     return this._taskId;
@@ -14,10 +12,78 @@ class Result{
     return this._status;
   }
   isSuccess(){
-    this._status === this._SUCCESS;
+    return this._status === constants.TASK_STATUS.SUCCESS;
   }
   isFailed(){
     return !this.isSuccess();
   }
 }
-module.exports=Result;
+class ComputeResult extends Result{
+  constructor(taskId,status,output,delta,usedGas,ethereumPayload,ethereumAddress,signature){
+    super(taskId,status);
+    this._output = output;
+    this._delta = delta;
+    this._usedGas = usedGas;
+    this._ethereumPayload = ethereumPayload;
+    this._ethereumAddress = ethereumAddress;
+    this._signature = signature;
+  }
+  static buildComputeResult(result){
+    let expected = ['taskId','status','output','delta','usedGas','ethereumPayload','ethereumAddress','signature'];
+    let isMissing = expected.some(attr=>{
+      return !(attr in result);
+    });
+    if(isMissing) return null;
+    return new ComputeResult(
+        result.taskId,
+        result.status,
+        result.output,
+        result.delta,
+        result.usedGas,
+        result.ethereumPayload,
+        result.ethereumAddress,
+        result.signature);
+  }
+  getOutput(){
+    return this._output;
+  }
+  getUsedGas(){return this._usedGas;}
+  getDelta(){return this._delta;}
+  getEthPayload(){return this._ethereumPayload;}
+  getEthAddr(){return this._ethereumAddress;}
+  getSignature(){return this._signature};
+}
+
+
+class DeployResult extends ComputeResult{
+  constructor(taskId,status,output,delta,usedGas,ethereumPayload,ethereumAddress,signature,preCodeHash){
+    super(taskId,status,output,delta,usedGas,ethereumPayload,ethereumAddress,signature);
+    this._preCodeHash = preCodeHash;
+  }
+  static buildDeployResult(result){
+    let expected = ['taskId','status','output','delta','usedGas','ethereumPayload','ethereumAddress','signature','preCodeHash'];
+    let isMissing = expected.some(attr=>{
+      return !(attr in result);
+    });
+    if(isMissing) return null;
+    return new DeployResult(
+        result.taskId,
+        result.status,
+        result.output,
+        result.delta,
+        result.usedGas,
+        result.ethereumPayload,
+        result.ethereumAddress,
+        result.signature,
+        result.preCodeHash
+    );
+  }
+  getPreCodeHash(){return this._preCodeHash;}
+}
+
+module.exports.Result = Result;
+module.exports.DeployResult = DeployResult;
+module.exports.ComputeResult = ComputeResult;
+
+
+
