73c33b7258545d1e395aa022c4ee382600bb0f58
Elichai Turkel <elichai@enigma.co>
2019-02-12 14:32:48 +0200

    DEV: Added To constants: Principal Node URI and GET_STATE_KEYS action

--- src/common/constants.js
+++ src/common/constants.js
@@ -46,6 +46,7 @@ module.exports.NODE_NOTIFICATIONS = {
   DEPLOY_SECRET_CONTRACT : 'dscontract', // deploySecretContract jsonrpc
   RECEIVED_NEW_RESULT : 'rnresult', // result updates receoved from the task results topic
   GET_TASK_STATUS : 'gtstatus', // get task status
+  GET_STATE_KEYS: 'getstatekeys', // PTT process
 };
 /** DO NOT CHANGE THE VALUES */
 module.exports.PROTOCOLS = {
@@ -151,6 +152,8 @@ module.exports.CORE_REQUESTS = {
   DeploySecretContract : 'DeploySecretContract', //jsonrpc request from remote use for deploying
   ComputeTask : 'ComputeTask', //jsonrpc request for compute task
   FailedTask : 'FailedTask', // failed task returned FROM core as a response to deploy/compute -> valid response should be commited.
+  GetPTTRequest: 'GetPTTRequest', // Get The PTT request from core with addresses.
+  PTTResponse: 'PTTResponse', // Give Core the response from the principal node.
 };
 
 /** Default configuration for JSON RPC Server
@@ -181,4 +184,9 @@ module.exports.ETHEREUM_EVENTS = {
   TaskCreation : 'TaskCreation',
   TaskSubmission : 'TaskSubmission',
   SecretContractDeployment : 'SecretContractDeployment',
+};
+
+
+module.exports.PRINCIPAL_NODE = {
+  uri: 'http://127.0.0.1:10101',
 };
