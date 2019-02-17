595703de7bebf13c1ad2cb8b26b5a88ec150e3a6
lenak25 <lenak25@gmail.com>
2019-02-17 14:31:26 +0200

    BLD: add missing mock file

---
+++ test/ethereum/EnigmaContractMock.js
@@ -0,0 +1,47 @@
+class EnigmaContractMock {
+
+  constructor() {
+    this._taskRecords = {};
+    this._epochSize = null;
+    this._eventListeners = {};
+    this._workersParams = [];
+  }
+
+  setTaskParams(taskId, deltaHash, outputHash, blockNumber, status) {
+    this._taskRecords[taskId] = {taskId: taskId,
+                                 deltaHash: deltaHash,
+                                 outputHash: outputHash,
+                                 blockNumber: blockNumber,
+                                 status: status};
+  }
+
+  setEpochSize(size) {
+    this._epochSize = size;
+  }
+
+  setWorkerParams(workerParams) {
+    this._workersParams = workerParams;
+  }
+
+  getTaskParams(taskId) {
+    return this._taskRecords[taskId];
+  }
+
+  getEpochSize() {
+    return this._epochSize;
+  }
+
+  getAllWorkerParams() {
+    return this._workersParams;
+  }
+
+  subscribe(eventName, filter, callback) {
+    this._eventListeners[eventName] = callback;
+  }
+
+  triggerEvent(eventName, event) {
+    this._eventListeners[eventName](null, event);
+  }
+}
+
+module.exports = EnigmaContractMock;
