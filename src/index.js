ce3320a6fdf8cbd67a293682136df9b7d6248455
Isan-Rivkin <isanrivkin@gmail.com>
2019-02-17 15:15:09 +0200

    MAINT: minor change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+module.exports.Builder = require('../src/main_controller/EnvironmentBuilder');
+module.exports.cryptography  = require('../src/common/cryptography');
 module.exports.Utils = {
   nodeUtils : require('../src/common/utils'),
   dbUtils : require('../common/DbUtils')
 };
-module.exports.Builder = require('../src/main_controller/EnvironmentBuilder');
-module.exports.cryptography  = require('../src/common/cryptography');
