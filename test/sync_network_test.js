3468428f17381aa5f0832a037fe6c88fb44c1c1c
Isan-Rivkin <isanrivkin@gmail.com>
2019-02-19 20:02:23 +0200

    TST: working on system test for sync

--- test/sync_network_test.js
+++ test/sync_network_test.js
@@ -3,14 +3,25 @@ const tree = require('./test_tree').TEST_TREE.sync_network;
 const constants = require('../src/common/constants');
 const testBuilder = require('./testUtils/quickBuilderUtil');
 const testUtils = require('./testUtils/utils');
-
+const DeployTask = require('../src/worker/tasks/DeployTask');
+const Result = require('../src/worker/tasks/Result');
 describe('sync_network_tests',()=> {
-  it('#1 Should test w1 Publish a task and w2 receive it', async function() {
+  /**
+   * plan:
+   * create 15 nodes. y? idk
+   * chose 1 worker node
+   * chose 1 verifier node
+   * - worker node publishes deployment task
+   * - all nodes get the task
+   * - verifier node request to find providers for that ecid
+   * - verify that it returns 14 potential providers => this means everyone synched and announced properly.
+   * */
+  it('#1 should test that everyone announced after task result published to the p2p', async function() {
     if (!tree['all'] || !tree['#1']) {
       this.skip();
     }
     return new Promise(async resolve => {
-      let {peers,bNode} = await testBuilder.createN(2);
+      let {peers,bNode} = await testBuilder.createN(15);
       await testUtils.sleep(5000);
       let bNodeController = bNode.mainController;
       let bNodeCoreServer = bNode.coreServer;
@@ -29,7 +40,41 @@ describe('sync_network_tests',()=> {
         await testUtils.rm_Minus_Rf(bPath);
         resolve();
       };
+      // create deploy task
+      let task = getDeployResultTask();
+      let workerPeer = peers[0];
+      let verifierPeer = peers[1];
+      // TODO:: HW CONTIBUE FROM HERE 
       await stopTest();
     });
   });
 });
+
+
+function getDeployResultTask(){
+  let taskRawObj = {
+    taskId : '0x0033105ed3302282dddd38fcc8330a6448f6ae16bbcb26209d8740e8b3d28538',
+    preCode : [0,1,2,3,4,5,6,7,8,9,10,11,22,11,33,22,55,66,99,66,33,66,33,66,33,66,33],
+    encryptedArgs : 'kjidejkdjeu83832uewhriowehjuehyhejdekioeuisdrhjfdrkhjfesdjksfdljkoifdkliutrjiukerfjk',
+    encryptedFn : 'kjhbsarglikujrhehihiuhihikkkkkkkkkkkkjdhfyueuehen',
+    userDHKey :  '5587fbc96b01bfe6482bf9361a08e84810afcc0b1af72a8e4520f98771ea1080681e8a2f9546e5924e18c047fa948591dba098bffaced50f97a41b0050bdab99',
+    gasLimit : 1200,
+    contractAddress : '0x0033105ed3302282dddd38fcc8330a6448f6ae16bbcb26209d8740e8b3d28538'
+  };
+  let deployTask = DeployTask.buildTask(taskRawObj);
+  // create result
+  let resultRawObj = {
+    taskId : deployTask.getTaskId(),
+    status : constants.TASK_STATUS.SUCCESS,
+    output : [123,22,4,55,66],
+    delta : {index : 2, data : [96,22,4,55,66,88]},
+    usedGas : 213,
+    ethereumPayload : [233,46,78],
+    ethereumAddress : 'cc353334487696ebc3e15411e0b106186eba3c0c',
+    signature : [233,43,67,54],
+    preCodeHash : '87c2d362de99f75a4f2755cdaaad2d11bf6cc65dc71356593c445535ff28f43d'
+  };
+  let result = Result.DeployResult.buildDeployResult(resultRawObj);
+  deployTask.setResult(result);
+  return deployTask;
+}
