f071e14d73b5a3261482cc59797197bf94bd0739
Isan-Rivkin <isanrivkin@gmail.com>
2019-02-25 13:08:53 +0200

    DOC: added comments

--- src/worker/controller/actions/proxy/GetStatusProxyAction.js
+++ src/worker/controller/actions/proxy/GetStatusProxyAction.js
@@ -1,6 +1,11 @@
 const constants = require('../../../../common/constants');
 const Envelop = require('../../../../main_controller/channels/Envelop');
-
+/**
+ * This action takes a taskId and checks if the result + status exists locally.
+ * if true: returns back the envelop
+ * else:
+ * ROUTE_BLOCKING_RPC (i.e goes to the network and looks for the worker)
+ * */
 class GetStatusProxyAction {
   constructor(controller) {
     this._controller = controller;
@@ -8,7 +13,6 @@ class GetStatusProxyAction {
   async execute(requestEnvelop) {
     let taskId = requestEnvelop.content().taskId;
     try{
-      // dont pass this errors here !!!!
       let result = await this._controller.asyncExecCmd(constants.NODE_NOTIFICATIONS.GET_TASK_RESULT,{taskId:taskId});
       const responseEnvelop = new Envelop(requestEnvelop.id(), {result: result.getStatus() , output : result.getOutput()}, requestEnvelop.type());
       this._controller.communicator().send(responseEnvelop);
