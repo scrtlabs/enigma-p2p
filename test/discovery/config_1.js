2e7d763dd0735c96e31301a8b873c7804c09974b
Isan-Rivkin <isanrivkin@gmail.com>
2019-03-12 15:53:54 +0200

    TST: prepeare env for tests

---
+++ test/discovery/config_1.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "logger" : {
+    "level" : "debug", // log level
+    "cli" : true, // output to std
+    "file" : false // output to file, if set path then will save to file else none.
+  },
+  "node" : {
+    "network" : {
+      "port" : "0", // if 0 then chose random port,
+      "multiAddrs" : ["/ip4/0.0.0.0/tcp/"],
+      // TODO:: ignored because of constants/namespace
+      "namespace" : "ipfs",
+      "bootstrapNodes" : ["/ip4/0.0.0.0/tcp/10300/ipfs/QmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm"],
+      // TODO:: ignored because of constants/CONSISTENT_DISCOVERY_PARAMS
+      "consistentDiscovery" : {
+        "persistent" : false, // do persistent discovery i.e every disconnect below lowDht
+        "delay": 500, // delay between each attempt
+        "maxRetry": 10, // max number of retires to reach optimal dht
+        "timeout": 100000 // maxtimeout before canceling discovery
+      },
+      // TODO:: ignored because of constants/DHT_STATUS
+      "optimalDht" : 8, // optimal number of outbound connections
+      "criticalLowDht" : 5 // whenever currentPeers < criticalLowDht -> should get more peers ASAP
+    },
+    "idPath" : null, // load PeerId, if null-> create one
+    // TODO:: ignored currently cuz of implementation
+    "id" : null, // either idPath or id -> actuall object here are acceptable if both are set, idPath is the default
+    // TODO:: ignored libp2p-bundle
+    "isDiscover" : true, // should do discovery ?
+    // the inner task manager of the node controller
+    "taskManager" : {
+      "dbPath" : null // the db path for storage, if null saves in default
+    },
+    // epoch related config
+    "principalNode" : {
+      "uri" : null //principal node url,  default if null
+    }
+  },
+  // IPC
+  "core" : {
+    "uri" : "tcp://127.0.0.1:5552" // ipc uri
+  },
+  // JsonRpc config
+  "proxy" : {
+    "withProxy" : true, // default serve as a proxy node
+    "port" : null // integer or null will default in constants
+  },
+  // Ethereum related configuration
+  "ethereum" :{
+    //default use ethereum or not
+    "withEthereum" : false,
+    // websocket provider
+    "ethereumWebsocketProvider" : "",
+    // enigma contract address
+    "enigmaContractAddress" : ""
+  },
+  // TODO:: CURRENTLY IGNORED
+  "dev:":{
+    "truffleDir" : ""
+  }
+};
---
+++ test/discovery/persistent_discovery_test.js
@@ -0,0 +1,48 @@
+const EnvironmentBuilder = require('../../src/main_controller/EnvironmentBuilder');
+const CoreServer = require('../../src/core/core_server_mock/core_server');
+const tree = require('../test_tree').TEST_TREE.single_config;
+const expect = require('expect');
+const assert = require('assert');
+const MainController = require('../../src/main_controller/FacadeController');
+const testUtils = require('../testUtils/utils');
+const path = require('path');
+const ID_B_PATH = path.join(__dirname, './id-l');
+const jayson = require('jayson');
+
+function getRpcClient(port){
+  return jayson.client.http('http://localhost:' + port);
+}
+function getConfig() {
+  return require('./config_1');
+}
+function getBootsrapConfig() {
+  let c =require('./config_2_bootstrap');
+  c.node.idPath = ID_B_PATH;
+  return c;
+}
+
+function getCoreServer(uri){
+  coreServer = new CoreServer();
+  coreServer.runServer(uri);
+  return coreServer;
+}
+
+describe('persistent_discovery_tests',()=> {
+
+  it('#1 Should do persistent discovery', async function() {
+    if (!tree['all'] || !tree['#1']) {
+      this.skip();
+    }
+    const c = getConfig();
+    return new Promise(async resolve => {
+      let coreServer = getCoreServer(c.core.uri);
+      let mainController = await EnvironmentBuilder.buildFromSingle(c);
+      expect(mainController).toEqual(expect.anything());
+      assert(mainController instanceof MainController, 'not main controller');
+      await mainController.shutdownSystem();
+      coreServer.disconnect();
+      resolve();
+    });
+  });
+});
+
