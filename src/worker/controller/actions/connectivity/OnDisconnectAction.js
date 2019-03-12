052ce2596d3386ce0c08afc86d070a6b63e24924
Isan-Rivkin <isanrivkin@gmail.com>
2019-03-12 17:58:48 +0200

    BUG: temporary fixing bug

--- src/worker/controller/actions/connectivity/OnDisconnectAction.js
+++ src/worker/controller/actions/connectivity/OnDisconnectAction.js
@@ -11,16 +11,20 @@ class OnDisconnectAction{
   }
   async execute(params){
     // check if persisting
+    console.log("0- @@@@@@@@@@@@@@@@@@@@@ w000000000000000000000000t @@@@@@@@@@@@@@@@@@@")
     let shouldPersist = constants.CONSISTENT_DISCOVERY_PARAMS.SHOULD_PERSIST;
     if(this._controller.getExtraConfig().discovery){
       shouldPersist = this._controller.getExtraConfig().discovery.persistent;
+      console.log("1- @@@@@@@@@@@@@@@@@@@@@ w000000000000000000000000t @@@@@@@@@@@@@@@@@@@ + " + shouldPersist)
     }
     if(shouldPersist && !this._isRunning){
+      console.log("2- @@@@@@@@@@@@@@@@@@@@@ w000000000000000000000000t @@@@@@@@@@@@@@@@@@@")
       // check if critical dht
       let connections = await this._controller.asyncExecCmd(constants.NODE_NOTIFICATIONS.GET_HANDSHAKE_NUM);
       if(connections && connections.outbounds){
         if(connections.outbounds.length < constants.DHT_STATUS.CRITICAL_LOW_DHT_SIZE){
           // do discovery
+          console.log("3- @@@@@@@@@@@@@@@@@@@@@ w000000000000000000000000t @@@@@@@@@@@@@@@@@@@")
           this.toggleIsRunning();
           this._controller.execCmd(constants.NODE_NOTIFICATIONS.CONSISTENT_DISCOVERY,{
             delay : constants.CONSISTENT_DISCOVERY_PARAMS.DELAY,
