8ad99c3a67b777ed62a82d7210afa981ba28d8a8
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-04-03 12:48:39 -0500

    MAINT: syncing across branches

--- src/ethereum/EthereumVerifier.js
+++ src/ethereum/EthereumVerifier.js
@@ -334,8 +334,8 @@ class EthereumVerifier {
   _verifySelectedWorker(secretContractAddress, workerAddress, params) {
     let result = {error: null, isVerified: true};
     let selectedWorker = EthereumVerifier.selectWorkerGroup(secretContractAddress, params, 1)[0];
-    selectedWorker = nodeUtils.remove0x(selectedWorker.signer.toLowerCase());  // To enable in update to the contract
-    if (selectedWorker.signer !== workerAddress) {
+    selectedWorker = nodeUtils.remove0x(selectedWorker.toLowerCase());  // To enable in update to the contract
+    if (selectedWorker !== workerAddress) {
       const err = new errors.WorkerSelectionVerificationErr("Not the selected worker for the " + secretContractAddress + " task");
       result.error = err;
       result.isVerified = false;
