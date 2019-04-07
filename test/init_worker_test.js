c9847d6da410235e6ac868992a1189ee6f5f4985
Avishai Weingarten <avishai@enigma.co>
2019-04-07 19:03:51 +0300

    MAINT: moved common test functions to test utils

--- test/init_worker_test.js
+++ test/init_worker_test.js
@@ -2,8 +2,6 @@ const tree = require('./test_tree').TEST_TREE.init_worker;
 const assert = require('assert');
 const testBuilder = require('./testUtils/quickBuilderUtil');
 const testUtils = require('./testUtils/utils');
-const DbUtils = require('../src/common/DbUtils');
-const DB_PROVIDER = require('../src/core/core_server_mock/data/provider_db');
 const noLoggerOpts = {
   bOpts : {
     withLogger : false,
@@ -27,37 +25,6 @@ const stopTest = async (peers,bNodeController,bNodeCoreServer,resolve)=>{
   resolve();
 };
 
-const PROVIDERS_DB_MAP = transformStatesListToMap(DB_PROVIDER);
-
-async function setEthereumState(api, web3, workerAddress, workerEnclaveSigningAddress) {
-  for (const address in PROVIDERS_DB_MAP) {
-    const secretContractData = PROVIDERS_DB_MAP[address];
-    const addressInByteArray = address.split(',').map(function(item) {
-      return parseInt(item, 10);
-    });
-
-    const hexString = '0x' + DbUtils.toHexString(addressInByteArray);
-    const codeHash = web3.utils.keccak256(secretContractData[-1]);
-    const firstDeltaHash = web3.utils.keccak256(secretContractData[0]);
-    const outputHash = web3.utils.randomHex(32);
-    const gasUsed = 5;
-    const optionalEthereumData = '0x00';
-    const optionalEthereumContractAddress = '0x0000000000000000000000000000000000000000';
-    await api.deploySecretContract(hexString, codeHash, codeHash, firstDeltaHash, optionalEthereumData,
-      optionalEthereumContractAddress, gasUsed, workerEnclaveSigningAddress, {from: workerAddress});
-    let i = 1;
-
-    while (i in secretContractData) {
-      const taskId = web3.utils.randomHex(32);
-      const delta = secretContractData[i];
-      const stateDeltaHash = web3.utils.keccak256(delta);
-      await api.commitReceipt(hexString, taskId, stateDeltaHash, outputHash, optionalEthereumData, optionalEthereumContractAddress, gasUsed,
-        workerEnclaveSigningAddress, {from: workerAddress});
-      i++;
-    }
-  }
-}
-
 async function prepareEthData(controller) {
   let api = controller.getNode().ethereum().api();
   const accounts = await api.w3().eth.getAccounts();
@@ -69,30 +36,8 @@ async function prepareEthData(controller) {
   await api.register(workerEnclaveSigningAddress, workerReport, signature, {from: workerAddress});
   await api.deposit(workerAddress, depositValue, {from: workerAddress});
   await api.login({from: workerAddress});
-  await setEthereumState(api, api.w3(), workerAddress, accounts[1]);
+  await testUtils.setEthereumState(api, api.w3(), workerAddress, accounts[1]);
   await testUtils.sleep(2000);
-}
-
-function transformStatesListToMap(statesList) {
-  const statesMap = {};
-  for (let i = 0; i < statesList.length; ++i) {
-    const address = statesList[i].address;
-    if (!(address in statesMap)) {
-      statesMap[address] = {};
-    }
-    const key = statesList[i].key;
-    const delta = statesList[i].data;
-    statesMap[address][key] = delta;
-  }
-  return statesMap;
-}
-
-function prepareSyncTestData() {
-  const res = {};
-
-  res.tips = [];
-  res.expected = PROVIDERS_DB_MAP;
-  return res;
 }
 
 // todo: create a DB for the coreServer which is stored in memory and
@@ -119,10 +64,10 @@ it('#1 run init and healthCheck', async function() {
     const accounts = await controller.getNode().ethereum().api().w3().eth.getAccounts();
     const workerAddress = accounts[0];
     coreServer.setSigningKey(workerAddress);
-    let data = prepareSyncTestData();
+    let noTipsReceiver = [];
     bNodeCoreServer.setProvider(true);
     await bNodeController.getNode().asynctryAnnounce();
-    coreServer.setReceiverTips(data.tips);
+    coreServer.setReceiverTips(noTipsReceiver);
     await testPeer.mainController.getNode().asyncInitializeWorkerProcess({amount: 50000});
     // await testUtils.sleep(2000);
     let hc = await testPeer.mainController.healthCheck();
