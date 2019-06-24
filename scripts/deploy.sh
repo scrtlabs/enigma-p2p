6c76d064133133b5e477e75f0a9dfd7759505723
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-06-24 15:36:04 -0600

    MAINT: tagging docker images depending on branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 git clone https://github.com/enigmampc/discovery-docker-network.git
 cd discovery-docker-network/enigma-p2p
-docker build --build-arg GIT_BRANCH_P2P=$TRAVIS_BRANCH -t enigmampc/enigma_p2p:latest --no-cache .
+
+if [[ ${TRAVIS_BRANCH} == "master" ]]; then
+	TAG=latest
+else
+	# ${TRAVIS_BRANCH} == "develop"
+	TAG=develop
+fi
+
+docker build --build-arg GIT_BRANCH_P2P=$TRAVIS_BRANCH -t enigmampc/enigma_p2p:${TAG} --no-cache .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push enigmampc/enigma_p2p:latest
+docker push enigmampc/enigma_p2p:${TAG}
