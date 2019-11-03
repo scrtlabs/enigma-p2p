0be35054c2f4c0e69c61ecdbfe9de358c9d491ae
assafmo <assaf.morami@gmail.com>
2019-11-03 15:15:44 +0200

    writer test skeleton

---
+++ test/ethereum/EnigmaContractProductionWriterAPI_test.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const assert = require('assert');
+const EnigmaContractProductionWriterAPI = require(path.join(__dirname, '../../src/ethereum/EnigmaContractProductionWriterAPI'));
+
+describe('Wait for x confirmations', () => {
+    describe('Writer', () => {
+        it('register', async () => {
+
+        })
+        it("deposit", async () => { })
+        it("selfDeposit", async () => { })
+        it("withdraw", async () => { })
+        it("deploySecretContract", async () => { })
+        it("login", async () => { })
+        it("logout", async () => { })
+        it("commitReceipt", async () => { })
+        it("commitTaskFailure", async () => { })
+        it("deploySecretContractFailure", async () => { })
+    })
+})
