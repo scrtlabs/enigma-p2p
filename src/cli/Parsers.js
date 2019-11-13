8316a8eecfe90b87ab2aa4cd06afc4312152a6a7
Moshe Demri <mdemri@gmail.com>
2019-11-13 15:40:43 +0200

    B3  bootstrapper

--- src/cli/Parsers.js
+++ src/cli/Parsers.js
@@ -1,6 +1,6 @@
 module.exports.list = function(val, params) {
-  const parseVal =val.split(',');
-  parseVal.forEach((ma)=>{
+  const parseVal = val.split(',');
+  parseVal.forEach((ma) => {
     let toReplace = '';
     let val = '';
     if (ma === 'B1') {
@@ -11,6 +11,10 @@ module.exports.list = function(val, params) {
       val = 'B2';
       toReplace = params.B2Addr;
     }
+    if (ma === 'B3') {
+      val = 'B3';
+      toReplace = params.B3Addr;
+    }
 
     const idx = parseVal.indexOf(val);
     parseVal[idx] = toReplace;
@@ -20,33 +24,39 @@ module.exports.list = function(val, params) {
   return parseVal;
 };
 
-module.exports.nickname = function(val,params) {
-  const parsedVal =val.toString();
+module.exports.nickname = function(val, params) {
+  const parsedVal = val.toString();
   params.configObject.nickname = parsedVal;
   params.changedKeys.push('nickname');
   return parsedVal;
 };
 
 module.exports.port = function(val, params) {
-  let parseVal =val.toString();
+  let parseVal = val.toString();
   if (parseVal === 'B1') {
     parseVal = params.B1Port;
   }
   if (parseVal === 'B2') {
     parseVal = params.B2Port;
   }
+  if (parseVal === 'B3') {
+    parseVal = params.B3Port;
+  }
   params.configObject.port = parseVal;
   params.changedKeys.push('port');
   return parseVal;
 };
 
-module.exports.idPath = function(val,params) {
+module.exports.idPath = function(val, params) {
   let parsedVal = val.toString();
   if (parsedVal === 'B1') {
     parsedVal = params.B1Path;
   }
   if (parsedVal === 'B2') {
     parsedVal = params.B2Path;
+  }
+  if (parsedVal === 'B3') {
+    parsedVal = params.B3Path;
   }
 
   params.configObject.idPath = parsedVal;
@@ -60,14 +70,12 @@ module.exports.idPath = function(val,params) {
 //     return val;
 //   };
 
-module.exports.opener= "  ______       _                         _____ ___  _____  \n" +
-    " |  ____|     (_)                       |  __ \\__ \\|  __ \\ \n" +
-    " | |__   _ __  _  __ _ _ __ ___   __ _  | |__) | ) | |__) |\n" +
-    " |  __| | '_ \\| |/ _` | '_ ` _ \\ / _` | |  ___/ / /|  ___/ \n" +
-    " | |____| | | | | (_| | | | | | | (_| | | |    / /_| |     \n" +
-    " |______|_| |_|_|\\__, |_| |_| |_|\\__,_| |_|   |____|_|     \n" +
-    "                  __/ |                                    \n" +
-    "                 |___/                                     ";
-
-
+module.exports.opener = '  ______       _                         _____ ___  _____  \n' +
+  ' |  ____|     (_)                       |  __ \\__ \\|  __ \\ \n' +
+  ' | |__   _ __  _  __ _ _ __ ___   __ _  | |__) | ) | |__) |\n' +
+  ' |  __| | \'_ \\| |/ _` | \'_ ` _ \\ / _` | |  ___/ / /|  ___/ \n' +
+  ' | |____| | | | | (_| | | | | | | (_| | | |    / /_| |     \n' +
+  ' |______|_| |_|_|\\__, |_| |_| |_|\\__,_| |_|   |____|_|     \n' +
+  '                  __/ |                                    \n' +
+  '                 |___/                                     ';
 
