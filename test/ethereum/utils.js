c40fe97b2508c9bea05a2cf578d8a28d27d430f1
assafmo <assaf.morami@gmail.com>
2019-11-13 10:50:32 +0200

    fix getEthereumBlockNumber undefined inside advanceXConfirmations

--- test/ethereum/utils.js
+++ test/ethereum/utils.js
@@ -130,11 +130,12 @@ module.exports.transformStatesListToMap = (statesList) => {
 module.exports.PROVIDERS_DB_MAP = this.transformStatesListToMap(DB_PROVIDER);
 
 
-module.exports.getEthereumBlockNumber = function getEthereumBlockNumber(web3) {
+function getEthereumBlockNumber(web3) {
   return new Promise((resolve, reject) => {
     web3.eth.getBlockNumber((error, data) => error ? reject(error) : resolve(data));
   });
 };
+module.exports.getEthereumBlockNumber = getEthereumBlockNumber;
 
 module.exports.advanceXConfirmations = async function (web3, confirmations = constants.MINIMUM_CONFIRMATIONS) {
   let initialEthereumBlockNumber = await getEthereumBlockNumber(web3);
