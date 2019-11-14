c27add1f364c461f5345b596f89b3b64cd78afea
assafmo <assaf.morami@gmail.com>
2019-11-14 16:31:15 +0200

    "Verify new compute task verification action" test fix promise weirdness

--- test/ethereum/eth_verifier_test.js
+++ test/ethereum/eth_verifier_test.js
@@ -3,10 +3,10 @@ const TEST_TREE = require('../test_tree').TEST_TREE;
 const ethTestUtils = require('./utils');
 const ControllerBuilder = require('../testUtils/quickBuilderUtil');
 const EthereumAPIMock = require('./EthereumAPIMock');
-const ComputeTask  = require('../../src/worker/tasks/ComputeTask');
-const DeployTask  = require('../../src/worker/tasks/DeployTask');
-const ComputeResult  = require('../../src/worker/tasks/Result').ComputeResult;
-const DeployResult  = require('../../src/worker/tasks/Result').DeployResult;
+const ComputeTask = require('../../src/worker/tasks/ComputeTask');
+const DeployTask = require('../../src/worker/tasks/DeployTask');
+const ComputeResult = require('../../src/worker/tasks/Result').ComputeResult;
+const DeployResult = require('../../src/worker/tasks/Result').DeployResult;
 const constants = require('../../src/common/constants');
 const cryptography = require('../../src/common/cryptography');
 const errors = require('../../src/common/errors');
@@ -15,13 +15,13 @@ const web3Utils = require('web3-utils');
 
 // TODO: lena: THIS TESTS SUITE SHOULD USE REAL ETHEREUM, ONCE CONTRACT UPDATED
 
-describe('Eth verifier tests', function() {
+describe('Eth verifier tests', function () {
 
   async function init(isDeploy, taskCreation) {
     const builder = await ControllerBuilder.createNode();
     const controller = builder.mainController;
 
-    let {params, expectedAddress, expectedParams, secretContractAddress, epochSize} = ethTestUtils.createDataForSelectionAlgorithm();
+    let { params, expectedAddress, expectedParams, secretContractAddress, epochSize } = ethTestUtils.createDataForSelectionAlgorithm();
     const ethereumAPI = new EthereumAPIMock(controller.getNode().logger());
 
     ethereumAPI.api().setEpochSize(epochSize);
@@ -41,16 +41,16 @@ describe('Eth verifier tests', function() {
       taskStatus = constants.ETHEREUM_TASK_STATUS.RECEIPT_VERIFIED;
       taskData = ethTestUtils.createDataForTaskSubmission();
       if (isDeploy) {
-        ethereumAPI.api().setContractParams(secretContractAddress, taskData.outputHash, {0: taskData.deltaHash});
+        ethereumAPI.api().setContractParams(secretContractAddress, taskData.outputHash, { 0: taskData.deltaHash });
         const delta = taskData.delta;
         const key = 0;
-        taskData.delta = {data: delta, key: key};
+        taskData.delta = { data: delta, key: key };
       }
       else {
-        ethereumAPI.api().setContractParams(secretContractAddress, null, {1: taskData.deltaHash}, {0: taskData.outputHash});
+        ethereumAPI.api().setContractParams(secretContractAddress, null, { 1: taskData.deltaHash }, { 0: taskData.outputHash });
         const delta = taskData.delta;
         const key = 1;
-        taskData.delta = {data: delta, key: key};
+        taskData.delta = { data: delta, key: key };
       }
     }
 
@@ -100,20 +100,21 @@ describe('Eth verifier tests', function() {
       coreServer: coreServer,
       dbPath: builder.tasksDbPath,
       taskData: taskData,
-      gasLimit: gasLimit};
+      gasLimit: gasLimit
+    };
   }
 
-  it('Verify new deploy task verification action', async function() {
+  it('Verify new deploy task verification action', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#1']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(true, true);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(true, true);
 
       // stop the test
-      const stopTest = async ()=>{
+      const stopTest = async () => {
         await controller.shutdownSystem();
         coreServer.disconnect();
         resolve();
@@ -121,47 +122,40 @@ describe('Eth verifier tests', function() {
 
       const task = DeployTask.buildTask(taskData);
       const res = await controller.getNode().asyncExecCmd(
-        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, {task: task});
+        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, { task: task });
       assert.strictEqual(res, true);
       await stopTest();
     });
   });
 
-  it('Verify new compute task verification action', async function() {
+  it('Verify new compute task verification action', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#2']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(false, true);
+    let { controller, coreServer, dbPath, taskData, gasLimit } = await init(false, true);
 
-      // stop the test
-      const stopTest = async ()=>{
-        await controller.shutdownSystem();
-        coreServer.disconnect();
-        resolve();
-      };
+    const task = ComputeTask.buildTask(taskData);
+    const res = await controller.getNode().asyncExecCmd(
+      constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, { task: task });
+    await controller.shutdownSystem();
+    coreServer.disconnect();
 
-      const task = ComputeTask.buildTask(taskData);
-      const res = await controller.getNode().asyncExecCmd(
-        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, {task: task});
-      assert.strictEqual(res, true);
-      await stopTest();
-    });
+    assert.strictEqual(res, true);
   });
 
-  it('Compute task verification action wrong worker address', async function() {
+  it('Compute task verification action wrong worker address', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#3']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(false, true);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(false, true);
 
       // stop the test
-      const stopTest = async ()=>{
+      const stopTest = async () => {
         await controller.shutdownSystem();
         coreServer.disconnect();
         resolve();
@@ -171,23 +165,23 @@ describe('Eth verifier tests', function() {
 
       const task = ComputeTask.buildTask(taskData);
       const res = await controller.getNode().asyncExecCmd(
-        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, {task: task});
+        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, { task: task });
       assert.strictEqual(res, false);
       await stopTest();
     });
   });
 
-  it('Deploy task verification action ethereum exception', async function() {
+  it('Deploy task verification action ethereum exception', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#4']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(true, true);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(true, true);
 
       // stop the test
-      const stopTest = async ()=>{
+      const stopTest = async () => {
         await controller.shutdownSystem();
         coreServer.disconnect();
         resolve();
@@ -197,20 +191,20 @@ describe('Eth verifier tests', function() {
 
       const task = DeployTask.buildTask(taskData);
       const res = await controller.getNode().asyncExecCmd(
-        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, {task: task});
+        constants.NODE_NOTIFICATIONS.VERIFY_NEW_TASK, { task: task });
       assert.strictEqual(res, false);
       await stopTest();
     });
   });
 
-  it('Verify deploy task submission action', async function() {
+  it('Verify deploy task submission action', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#5']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(true, false);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(true, false);
 
       // stop the test
       const stopTest = async () => {
@@ -225,23 +219,25 @@ describe('Eth verifier tests', function() {
       };
 
       const task = DeployResult.buildDeployResult(taskData);
-      const rawMessage = Buffer.from(JSON.stringify({result: task.toDbJson(),
+      const rawMessage = Buffer.from(JSON.stringify({
+        result: task.toDbJson(),
         contractAddress: taskData.contractAddress,
-        type: constants.CORE_REQUESTS.DeploySecretContract}));
+        type: constants.CORE_REQUESTS.DeploySecretContract
+      }));
 
       controller.getNode().execCmd(
-        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, {params: {data: rawMessage}, callback: callback});
+        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, { params: { data: rawMessage }, callback: callback });
     });
   });
 
-  it('Verify compute task submission action', async function() {
+  it('Verify compute task submission action', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#6']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(false, false);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(false, false);
 
       // stop the test
       const stopTest = async () => {
@@ -256,23 +252,25 @@ describe('Eth verifier tests', function() {
       };
 
       const task = ComputeResult.buildComputeResult(taskData);
-      const rawMessage = Buffer.from(JSON.stringify({result: task.toDbJson(),
+      const rawMessage = Buffer.from(JSON.stringify({
+        result: task.toDbJson(),
         contractAddress: taskData.contractAddress,
-        type: constants.CORE_REQUESTS.ComputeTask}));
+        type: constants.CORE_REQUESTS.ComputeTask
+      }));
 
       controller.getNode().execCmd(
-        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, {params: {data: rawMessage}, callback: callback});
+        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, { params: { data: rawMessage }, callback: callback });
     });
   });
 
-  it('Deploy task submission action verification error', async function() {
+  it('Deploy task submission action verification error', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#7']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(true, false);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(true, false);
 
       // stop the test
       const stopTest = async () => {
@@ -289,23 +287,25 @@ describe('Eth verifier tests', function() {
       taskData.delta.data = web3Utils.randomHex(20);
 
       const task = DeployResult.buildDeployResult(taskData);
-      const rawMessage = Buffer.from(JSON.stringify({result: task.toDbJson(),
+      const rawMessage = Buffer.from(JSON.stringify({
+        result: task.toDbJson(),
         contractAddress: taskData.contractAddress,
-        type: constants.CORE_REQUESTS.DeploySecretContract}));
+        type: constants.CORE_REQUESTS.DeploySecretContract
+      }));
 
       controller.getNode().execCmd(
-        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, {params: {data: rawMessage}, callback: callback});
+        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, { params: { data: rawMessage }, callback: callback });
     });
   });
 
-  it('Compute task submission action ethereum exception', async function() {
+  it('Compute task submission action ethereum exception', async function () {
     const tree = TEST_TREE.ethereum_integration;
     if (!tree['all'] || !tree['#8']) {
       this.skip();
     }
 
-    return new Promise(async function(resolve) {
-      let {controller, coreServer, dbPath, taskData, gasLimit} = await init(false, false);
+    return new Promise(async function (resolve) {
+      let { controller, coreServer, dbPath, taskData, gasLimit } = await init(false, false);
 
       // stop the test
       const stopTest = async () => {
@@ -322,12 +322,14 @@ describe('Eth verifier tests', function() {
       controller.getNode().ethereum().api().triggerException();
 
       const task = ComputeResult.buildComputeResult(taskData);
-      const rawMessage = Buffer.from(JSON.stringify({result: task.toDbJson(),
+      const rawMessage = Buffer.from(JSON.stringify({
+        result: task.toDbJson(),
         contractAddress: taskData.contractAddress,
-        type: constants.CORE_REQUESTS.ComputeTask}));
+        type: constants.CORE_REQUESTS.ComputeTask
+      }));
 
       controller.getNode().execCmd(
-        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, {params: {data: rawMessage}, callback: callback});
+        constants.NODE_NOTIFICATIONS.RECEIVED_NEW_RESULT, { params: { data: rawMessage }, callback: callback });
     });
   });
 })
